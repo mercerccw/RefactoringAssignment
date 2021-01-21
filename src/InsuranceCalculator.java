78f62e7e074c6f192812dd2375873a1773f388eb
Clayton Mercer <mercerccw@gmail.com>
2021-01-21 10:24:14 -0500

    Extracting methods

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -2,14 +2,30 @@ public class InsuranceCalculator {
 
     public double calculateInsurance(double income) {
         if (income <= 10000) {
-            return income*0.365;
+            return income * 0.365;
         } else if (income <= 30000) {
-            return (income-10000)*0.2+35600;
+            return (income - 10000) * 0.2 + 35600;
         } else if (income <= 60000) {
-            return (income-30000)*0.1+76500;
+            return (income - 30000) * 0.1 + 76500;
         } else {
-            return (income-60000)*0.02+105600;
+            return calculateInsuranceVeryHigh(income);
         }
+    }
+
+    private double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    public int getConstant() {
+        return 105600;
+    }
+
+    public double getWeight() {
+        return 0.02;
+    }
+
+    public int getAdjustment() {
+        return 60000;
     }
 
 }
